1bb5e572862112859f99ab4221b47a51ba790d3d
BSP) Pengfei Li <lipf0407@thundersoft.com>
2016-05-25 18:15:14 +0800

    1

--- out_vexpress_3_16/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 9 */
-/* SMP */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#9 SMP Thu May 19 17:00:30 CST 2016"
-#define LINUX_COMPILE_BY "jack"
-#define LINUX_COMPILE_HOST "jack"
-#define LINUX_COMPILER "gcc version 4.6.3 (Ubuntu/Linaro 4.6.3-1ubuntu5) "
